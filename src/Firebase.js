74f6793356ed1000b989b59d9547d78f2515c123
yeshwanth <yashwanthb1556@gmail.com>
2022-04-14 21:50:56 -0700

    secured

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,12 +3,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7RhcrejKGklYriAg_-cHO46h1925dMhU",
-  authDomain: "vsev-25587.firebaseapp.com",
-  projectId: "vsev-25587",
-  storageBucket: "vsev-25587.appspot.com",
-  messagingSenderId: "650205937830",
-  appId: "1:650205937830:web:418366903437f1f2677467"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 const app = firebase.initializeApp(firebaseConfig);
 const db = app.firestore();
